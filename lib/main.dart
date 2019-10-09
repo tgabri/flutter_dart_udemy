2b7074fac9bb6607d2da400516a7a2e93b3ad853
Gabor <tgabri@gmail.com>
2019-10-09 08:18:30 +0100

    new file created, main text moved

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite color?',
+      'What is your favourite animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Test App'),
         ),
-        body: Text('Hello World'),
+        body: Column(
+          children: <Widget>[
+            Question(questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
